30b2d40e3c8de616fe332d46f7a8926f4d541847
Sverre Voje Blindheim <sverre.v.b@gmail.com>
2018-02-01 18:09:03 +0100

    Added map function. - Relocated the map function from map.html to the external js file.

--- js/base.js
+++ js/base.js
@@ -1,1 +1,11 @@
-asdasd
+function initMap() {
+  var bergen = {lat: 60.388395, lng: 5.323619};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: bergen
+  });
+  var marker = new google.maps.Marker({
+    position: bergen,
+    map: map
+  });
+}
