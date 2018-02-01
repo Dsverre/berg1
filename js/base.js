3c1604ab8b31216e754ff7073b16c4e965e129c8
Sverre Voje Blindheim <sverre.v.b@gmail.com>
2018-02-01 18:52:55 +0100

    Added another marker to map and infoboxes. - Added a marker for Brann stadion. - Added infoboxes to both markers.

--- js/base.js
+++ js/base.js
@@ -1,11 +1,53 @@
 function initMap() {
-  var bergen = {lat: 60.388395, lng: 5.323619};
+  var upihl = {lat: 60.388395, lng: 5.323619};
+  var stadion = {lat: 60.367101, lng: 5.357427};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 13,
-    center: bergen
+    center: upihl
   });
-  var marker = new google.maps.Marker({
-    position: bergen,
+
+  var contentString1 = '<div id="content">'+
+  '<div id="siteNotice">'+
+  '</div>'+
+  '<h1 id="firstHeading" class="firstHeading">Ulrikke Pihls Hus</h1>'+
+  '<div id="bodyContent">'+
+  '<p><b>Ulrikke Pihls hus</b> er et av mange universitetsbygg i Bergen sentrum.</p>'+
+  '</div>'+
+  '</div>';
+
+  var infowindow1 = new google.maps.InfoWindow({
+          content: contentString1
+  });
+
+  var marker1 = new google.maps.Marker({
+    position: upihl,
     map: map
   });
+
+  marker1.addListener('click', function() {
+    infowindow1.open(map, marker1);
+  });
+
+  var contentString2 = '<div id="content">'+
+  '<div id="siteNotice">'+
+  '</div>'+
+  '<h1 id="firstHeading" class="firstHeading">Brann Stadion</h1>'+
+  '<div id="bodyContent">'+
+  '<p><b>Brann stadion</b> huser byens Eliteserielag i fotball.</p>'+
+  '</div>'+
+  '</div>';
+
+  var infowindow2 = new google.maps.InfoWindow({
+          content: contentString2
+  });
+
+  var marker2 = new google.maps.Marker({
+    position: stadion,
+    map: map
+  });
+
+  marker2.addListener('click', function() {
+    infowindow2.open(map, marker2);
+  });
+
 }
