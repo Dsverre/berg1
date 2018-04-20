4c6cde056206465f7e6a07a0dbd7426a8226dd7f
Sverre Voje Blindheim <sverre.v.b@gmail.com>
2018-04-21 01:39:13 +0200

    Extensive commenting. - Added comment to most part of base.js

--- js/base.js
+++ js/base.js
@@ -1,26 +1,30 @@
-var markers = [];
-var sokeRes = [];
-var flag = 0;
-var toa = 0;
-var dataset;
-var map;
-var syntax;
+var markers = []; //Et array med alle markørene. Fylles i markAuto(), som kalles av intitList, via window.onload. Brukes senere for å skjule alle markørene før man via et midlertidig array viser kun de man har treff på.
+var sokeRes = []; //Et array som holder json-objekter man har truffet i søk. Brukes per nå av alle funksjoner som manipulerer datasettet. Vet ikke om helt det trenger å være globalt, se printRes().
+var flag = 0; //Et flagg som brukes av de booleanske funksjonene via flagCheck(), for å rense listen når man "unchecker". Er kanskje overflødig.
+var dataset; //Arrayet som holder alle JSON-objektene som sidene er bygget rundt. Klargjøres vha løftet hentUrl() utført av initList(). Blir ikke manifulert av noe per nå. Må ha .entries med begge datasettene, og de har dessverre forskjellige navn på egenskapene, derav disse if og else if'ene.
+var map; //Holder hele kartet? Måtte være global for at markørene skulle kunne skjules, men kan være overflødig.
+var syntax; //Brukes for å avgjøre hvilket datasett siden bruker. Blir satt via onload-sjekken.
 
+
+// Ved lasting av vinduet kjøres en skreddersydd funksjon.
+// Denne funksjonen starter med å splitte nettadressen ved /, før den lagrer det siste leddet av denne splitten som en variabel.
+// Deretter følger en sjekk av denne variabelen, hvor resultatet avgjør hvilket datasett som fødes til initList(), samt at syntax settes.
 window.onload = function() {
   var currentUrl = window.location.href;
   var page = currentUrl.split("/")[currentUrl.split("/").length-1];
   if(page == "toaletter.html") {
     initList("https://hotell.difi.no/api/json/bergen/dokart?");
-    syntax = "plassering";
+    syntax = "toaletter";
   }
   else if(page == "lekeplasser.html") {
      initList("https://hotell.difi.no/api/json/bergen/lekeplasser?");
-     syntax = "navn"
+     syntax = "leke"
   }
 
 }
 
-
+// Initiering av kartet. Her settes den globale variabelen map.
+// Zoom settes og koordinatene til et passende sted i Bergen sentrun styre sentrum av kartet.
 function initMap() {
 
 
@@ -30,6 +34,13 @@ function initMap() {
   });
 
 }
+
+
+// Løftet som henter datasettet.
+// Det returneres et løfte, som deretter kan manipuleres vha .then og .catch.
+//
+// Den skreddersydde(anonyme) funksjonen som løftet utfører er en HttpRequest.
+
 
 function hentUrl(url) {
     return new Promise(function(resolve, reject) {
@@ -48,27 +59,39 @@ function hentUrl(url) {
     });
 }
 
-
+// Funksjonen som er ryggraden i hele skriptet.
+// Her hentes datasettet, #liste elementet fylles av objektene datasettet inneholder og en sjekk avgjør syntaxen til kilden(holder).
+// Etter dette kjøres markAuto() for å initialisere markørene.
+//
+// For å få tak i selve datasettet, så må man gi det til den anonyme funksjonen(result).
+//
 function initList(url) {
 
 
   hentUrl(url).then(function (result) {
     dataset = JSON.parse(result);
     for(i = 0; i < dataset.entries.length; i++) {
-      if(syntax == "navn") var holder = dataset.entries[i].navn;
-      else if(syntax == "plassering") var holder = dataset.entries[i].plassering;
+      if(syntax == "leke") var holder = dataset.entries[i].navn;
+      else if(syntax == "toaletter") var holder = dataset.entries[i].toaletter;
       var node = document.createElement("LI");
       var textnode = document.createTextNode(i+1 + ": " + holder);
       node.appendChild(textnode);
       document.getElementById("liste").appendChild(node);
     }
-    flag = 0;
     markAuto();
 
   });
 }
 
-
+// Funksjonen som lager markører og infovindu(er).
+// Først lages innholdet til variabelen content. Her er en og en sjekk for å avgjøre kildesyntaksen til innholdet.
+// Koordinatene vi vil ha markøren på lagres, før markøren lages.
+// Markøren gis en posisjon, det sies hvilket kart den tilhører og vi gir markøren et label.
+// Vi lagrer også innholde i content, som ble opprettet i starten av funksjonen, i en vilkårlig navngitt variabel, content.
+// Slik kan vi få tak i den når vi etterpå skreddersyr en funksjon for å vise infovinduet, som limes på hver eneste markør via en click-listener.
+// Det er viktig at this brukes for å få tak i innholdet vi lagrer i markøren.
+//
+// Til slutt fylles markers-arrayet.
 function markAuto() {
 
 
@@ -78,21 +101,20 @@ function markAuto() {
 
       var sideNotice = document.createElement("div");
       sideNotice.setAttribute("class", "sideNotice");
-
       content.appendChild(sideNotice);
 
       var firstHeading = document.createElement("h1");
       firstHeading.setAttribute("id", "firstHeading");
-      if(syntax == "navn") var holder1 = dataset.entries[i].navn;
-      else if(syntax == "plassering") var holder1 = dataset.entries[i].plassering;
+      if(syntax == "leke") var holder1 = dataset.entries[i].navn;
+      else if(syntax == "toaletter") var holder1 = dataset.entries[i].plassering;
       var headingText = document.createTextNode((i+1) + ". " + holder1);
       firstHeading.appendChild(headingText);
       content.appendChild(firstHeading);
 
       var bodyContent = document.createElement("div");
       bodyContent.setAttribute("class", "bodyContent");
-      if(syntax == "navn") var holder2 = "";
-      else if(syntax == "plassering") var holder2 = dataset.entries[i].adresse;
+      if(syntax == "leke") var holder2 = "";
+      else if(syntax == "toaletter") var holder2 = dataset.entries[i].adresse;
       var paraText = document.createTextNode(holder2);
       bodyContent.appendChild(paraText);
       content.appendChild(bodyContent);
@@ -115,17 +137,24 @@ function markAuto() {
 }
 
 
+
+// Funksjon for å tømme #liste-elementet. Føles smått overflødig eller i det minste meget uelegant.
 function refresh() {
   while(document.getElementById("liste").hasChildNodes()) {
      document.getElementById("liste").removeChild(document.getElementById("liste").firstChild);
   };
 }
 
+
+// Funksjon som brukes av de fleste søkefunksjonene for å skrive en ny liste.
+// Per nå sjekket det et flagg for å tømme listen før man fyller den med resultatet av denne funksjonen, men føles uelegant.
+// Blir fødd med et sokeRes av de fleste funksjoner som bruker den, så kanskje kan sokeRes derfor gjøres lokal.
+// Bemerk at res, altså en lokal sokeRes, har blitt fyllt med JSON-objektene, noe som betyr at man ikke trnger .entries som ellers.
 function printRes(res) {
   for(i = 0; i < res.length; i++) {
     var node = document.createElement("LI");
-    if(syntax == "navn") var holder = res[i].navn;
-    else if(syntax == "plassering") var holder = res[i].plassering;
+    if(syntax == "leke") var holder = res[i].navn;
+    else if(syntax == "toaletter") var holder = res[i].plassering;
     var textnode = document.createTextNode(i+1 + ": " + holder);
     node.appendChild(textnode);
     document.getElementById("liste").appendChild(node);
@@ -133,6 +162,9 @@ function printRes(res) {
 }
 
 
+
+// Brukes av de fleste søkefunksjonene for å skjule alle markørene før de med treff skal vises.
+// Fødes ofte med true.
 function showMarkers(para) {
   for(i = 0; i < markers.length; i++) {
     markers[i].setVisible(para);
@@ -141,6 +173,12 @@ function showMarkers(para) {
 
 
 
+// Flaggsjekkfunksjonen.
+// Tanken er at når funksjoner har manipulert listen eller markørene, så skal flag settes til 1.
+// På denne måten kan man sjekke om noe har blitt manipulert, slik at dette kan resettes før et nytt søk.
+// Den er per nå et stykke unna full funksjonalitet.
+// De fleste funksjoner bør forholde seg til denne, og den bør også har kotroll over innholdet til mange variabler, om enn indirekte.
+// Enten det, eller så bør den erstattes med alternative løsninger eller lokale løsninger-
 function flagCheck() {
   if(flag == 1) {
     refresh();
@@ -150,11 +188,22 @@ function flagCheck() {
   };
 
 }
-//IndexOf + Include (js-funksjoner til å matche treff)
 
+
+
+//IndexOf + Include (js-funksjoner til å matche treff)
+//
+// Siste utgave av sokeFunk().
+// Ser nå at grunnen til at sokeRes er global, er for at man kan sjekke om den er tom før man fyller den med treff.
+// Kanskje dette kan unngås via flagCheck()?
+//
+// sokeRes[] og tempMarkers[] fylles med hhv JSON-objektene og de tilhørende markørene som søket resulterer i.
+// Skjuler alle markørene før kun de med treff vises igjen.
+//
+// Lista tømmes før den fylles med treffene via printRes. Her ses kode som kanksje kan erstatte eller tas opp i flagCheck().
 function sokeFunk() {
   if(document.getElementById("sokinput").value == "") return;
-
+  if(sokeRes.length != 0) sokeRes = [];
   var sokeParam = document.getElementById("sokinput").value.toUpperCase();
   var tempMarkers = [];
   for(i = 0; i < dataset.entries.length; i++) {
@@ -176,7 +225,9 @@ function sokeFunk() {
 }
 
 
-
+// Er trolig fullstendig overflødig.
+// Hele søkefunksjonen bør nok utføres av samme funksjon.
+// Jobb heller med å utvikle den.
 var treff;
 var adresser = [];
 function asokeFunk() {
@@ -200,17 +251,15 @@ function asokeFunk() {
 }
 
 
-
+// Uferdig funksjon for å sjekke hvilke toaletter som er åpne nå.
 var now;
-
 function openNow() {
  now = new Date();
 }
 
-
-
-
-
+//Funksjon for å vise representere hvilke objekter som er åpne på søndag.
+// Her brukes flagCheck() for å resette både lista og markørene og returnere hvis man allerede har "checket" checkboxen.
+// Ellers ganske intuitiv kode, dog noe gammel.
 function openSunday() {
   if(flagCheck() == true) return;
 
@@ -234,7 +283,8 @@ function openSunday() {
 }
 
 
-
+// Funksjon for å representere hvilke objekter som har dametoalett.
+// Nesten identisk som den over.
 function harDame() {
   if(flagCheck() == true) return;
 
@@ -257,6 +307,8 @@ function harDame() {
   flag = 1;
 }
 
+
+// Samme som de over, bare det handler om stellerom.
 function harStell() {
   if(flagCheck() == true) return;
 
